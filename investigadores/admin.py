5c99ec3ee49fb4ad4578bd263d9680f68aa67a34
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2023-08-10 09:41:22 -0600

    Register model

--- investigadores/admin.py
+++ investigadores/admin.py
@@ -3,10 +3,12 @@ from investigadores.models import (
     Investigador,
     NivelInvestigador,
     Investigacion,
-    SolicitudTrabajo)
+    SolicitudTrabajo,
+    CategoriaA)
 
 # Register your models here.
 admin.site.register(Investigador)
 admin.site.register(NivelInvestigador)
 admin.site.register(Investigacion)
 admin.site.register(SolicitudTrabajo)
+admin.site.register(CategoriaA)
